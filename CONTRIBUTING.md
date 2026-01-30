b83000e4e75cf5d30ad616b0d88135502770e1a7
jeman618 <jcisne23@calpoly.edu>
2026-01-30 09:32:25 -0800

    Added previous assignment and installed ESLint and prettier

---
+++ CONTRIBUTING.md
@@ -0,0 +1,1 @@
+We will figure out styling later
---
+++ packages/express-backend/backend.js
@@ -0,0 +1,116 @@
+// backend.js
+import express from "express";
+import cors from "cors";
+import userService from "./services/user-service.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// const users = {
+//   users_list: [
+//     {
+//       id: "xyz789",
+//       name: "Charlie",
+//       job: "Janitor"
+//     },
+//     {
+//       id: "abc123",
+//       name: "Mac",
+//       job: "Bouncer"
+//     },
+//     {
+//       id: "ppp222",
+//       name: "Mac",
+//       job: "Professor"
+//     },
+//     {
+//       id: "yat999",
+//       name: "Dee",
+//       job: "Aspring actress"
+//     },
+//     {
+//       id: "zap555",
+//       name: "Dennis",
+//       job: "Bartender"
+//     }
+//   ]
+// };
+
+dotenv.config();
+
+const { MONGO_CONNECTION_STRING } = process.env;
+
+mongoose.set("debug", true);
+mongoose
+  .connect(MONGO_CONNECTION_STRING + "users") // connect to DB 'users'
+  .catch((error) => console.log(error));
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  const { name, job } = req.query;
+  userService
+    .getUsers(name, job)
+    .then((result) => {
+      res.send({ users_list: result });
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id; // or req.params["id"]
+
+  userService.findUserById(id).then((result) => {
+    if (result) {
+      res.send(result);
+    } else {
+      res.status(404).send(`Not found: ${id}`);
+    }
+  });
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+
+  userService
+    .addUser(userToAdd)
+    .then((result) => {
+      res.status(201).send(result); // Content created
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+
+  userService
+    .findByIdAndDelete(id)
+    .then((user) => {
+      if (user) {
+        res.status(204).send(user); // No content
+      } else {
+        res.status(404).send(`Not found: ${id}`);
+      }
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+});
