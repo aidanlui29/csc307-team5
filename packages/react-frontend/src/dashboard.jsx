4950b619441141830f0e43eb55863873ca292fcd
Patrick <pzhou02@calpoly.edu>
2026-03-05 18:08:53 -0800

    search icon

--- packages/react-frontend/src/dashboard.jsx
+++ packages/react-frontend/src/dashboard.jsx
@@ -301,7 +301,7 @@ export default function Dashboard() {
             <div className="dash__controls">
               <div className="dash__searchMini">
                 <span className="dash__searchIcon">
-                <Search size={5} />
+                  <Search size={5} />
                 </span>
                 <input
                   className="dash__searchMiniInput"
