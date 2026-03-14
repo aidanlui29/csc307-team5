f0845005c76dd147a05d208ae8802008cfb5eb8b
Aidan Lui <adn4lui@gmail.com>
2026-03-14 14:18:23 -0700

    final

--- packages/react-frontend/src/setupTests.js
+++ packages/react-frontend/src/setupTests.js
@@ -1,6 +1,5 @@
 import "@testing-library/jest-dom";
 
-// Polyfill TextEncoder/TextDecoder for react-router (Jest/JSDOM)
 import { TextEncoder, TextDecoder } from "util";
 
 if (!global.TextEncoder) global.TextEncoder = TextEncoder;
